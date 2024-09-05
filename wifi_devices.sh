d6557255a4851d80e4bb40a4af2d6918dd23f87e
utnahs <s21111998s@gmail.com>
2024-09-05 18:21:27 +0545

    Update wifi_devices.sh

--- wifi_devices.sh
+++ wifi_devices.sh
@@ -3,11 +3,10 @@ getip() {
 
         ip address | grep 255 | cut -d '.' -f 3 
 }
-for ((i = 2 ; i < 255 ; i++)); 
-        do
-                ping -c 1 -s 0 -q 192.168.$(getip).$i &>/dev/null
+for ((i = 2 ; i < 255 ; i++)); do
+	ping -c 1 -s 0 -q 192.168.$(getip).$i &>/dev/null
 	if [ $? -eq 0 ]; then
-		echo "192.168.$(getip).$i"
+	echo "192.168.$(getip).$i"
 	fi
 done
 
